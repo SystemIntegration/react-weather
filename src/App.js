9967f869f75a84cf7a91f780119bf85573972b9d
nandini <nandinic@systemintegration.in>
2023-04-05 16:50:55 +0530

    Add some functionality and change whole app structure.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import PlaceIcon from '@mui/icons-material/Place';
-import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 import './App.css'
-import { BottomNavigation, Button, Chip, Paper, Typography } from '@mui/material';
+import { Button, Chip, FormControlLabel, Switch, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { styled } from '@mui/material/styles';
 
@@ -16,22 +14,23 @@ const CustomWidthTooltip = styled(({ className, ...props }) => (
   },
 });
 
+
 function App() {
-  const [searchText, setSearchText] = useState('mumbai')
+  const [searchText, setSearchText] = useState('Ahmedabad')
   const [cityData, setCityData] = useState([])
+  const [value, setValue] = useState(false)
   const [date, setDate] = useState(new Date());
+  const API_KEY = '7294a3e2e59505a06d9d4bb7dc1c3e57';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${API_KEY}&units=metric`;
 
   useEffect(() => {
     const timerID = setInterval(() => setDate(new Date()), 1000);
-
     return () => {
       clearInterval(timerID);
     };
   }, []);
 
-  const API_KEY = '7294a3e2e59505a06d9d4bb7dc1c3e57';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${API_KEY}&units=metric`;
-
+  //method for call API for weather
   useEffect(() => {
     const fatchAPI = async () => {
       const response = await fetch(url)
@@ -42,65 +41,102 @@ function App() {
     fatchAPI();
   }, [searchText])
 
+  //method for get value for change Celsius -> Fahrenheit or Fahrenheit -> Celsius
+  const getValue = () => {
+    if (value === false) {
+      setValue(true)
+    } else {
+      setValue(false)
+    }
+  }
+  //method for change temp value Celsius -> Fahrenheit or Fahrenheit -> Celsius
+  const getTempValue = (temp) => {
+    if (value === true) {
+      return ((temp * 9) / 5 + 32).toFixed(2);
+    } else {
+      return temp.toFixed(2);
+    }
+  }
+
+  const temp = cityData.main !== undefined && getTempValue(cityData.main.temp); // give temp value
+  const tempMax = cityData.main !== undefined && getTempValue(cityData.main.temp_max); // give maximum temp value
+  const tempMin = cityData.main !== undefined && getTempValue(cityData.main.temp_min); // give minimum temp value
+  const name = cityData.main !== undefined && cityData.name; // give city name value
+  const icon = cityData.weather !== undefined && cityData.weather[0].icon; // give icon value
+  const title = cityData.weather !== undefined && cityData.weather[0].main; // give tolltip value
+  const degValue = value === true ? "°F" : "°C"; // give degree value
+
+
   return (
     <>
-      <div className='mainBox' style={{ paddingTop: '1rem', color: 'floralwhite' }}>
+      <div className='mainBox' style={{ paddingTop: '1rem' }}>
         <div className='input' style={{ textAlign: 'center' }}>
           <input type="search" className="inputTagCSS" onChange={(e) => setSearchText(e.target.value)} />
         </div>
-
-        {cityData.main !== undefined ?
+        {cityData.main !== undefined &&
           <div>
-            <div>
-              <div className='Icon_Name' style={{ textAlign: 'end' }}>
-                <PlaceIcon style={{ color: 'red', fontSize: '5rem' }} />
-                <h3 style={{ paddingRight: '0.8rem', margin: '0' }}>
-                  {cityData.name}
-                </h3>
-                <h4 style={{ paddingRight: '0.8rem', margin: '0' }}>
-                  {date.toLocaleString()}
-                </h4>
-              </div>
+            <div style={{ position: 'absolute', right: '0', top: '0' }}>
+              <CustomWidthTooltip title={title} placement="top" arrow>
+                <Button>
+                  <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="" style={{ borderRadius: '50%', backgroundColor: 'aqua', height: '3rem' }} />
+                </Button>
+              </CustomWidthTooltip>
             </div>
-            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, opacity: '0.5', height: '10rem' }} elevation={3}>
-              <BottomNavigation
-                style={{ justifyContent: 'space-between', height: '3rem', backgroundColor: '#F3FEE8' }}
-              >
-                <div
-                  style={{ display: 'flex', justifyContent: 'center' }}
-                >
-                  <div style={{ paddingTop: '1rem' }}>
-                    <Typography style={{ marginLeft: '1rem', fontSize: '5rem' }}>
-                      {cityData.main.temp}°C
-                      <CustomWidthTooltip title={cityData.weather[0].description} placement="top" arrow>
-
-                        <Button>
-                          <img src={`https://openweathermap.org/img/wn/${cityData.weather[0].icon}@2x.png`} alt="" />
-                        </Button>
-                      </CustomWidthTooltip>
-                      |
-                    </Typography>
-                  </div>
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <div
-                      style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', paddingTop: '1rem' }}>
-                      <Chip label="Minimum" style={{ background: 'black', fontSize: '1rem', color: 'white', width: '9rem' }} />
-                      <Typography style={{ fontSize: '5rem' }}>
-                        {cityData.main.temp_min}°C
-                      </Typography>
-                    </div>
-                    <div
-                      style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', marginLeft: '2rem', paddingTop: '1rem' }}>
-                      <Chip label="Maximum" style={{ background: 'black', fontSize: '1rem', color: 'white', width: '9rem' }} />
-                      <Typography style={{ fontSize: '5rem' }}>
-                        {cityData.main.temp_max}°C
-                      </Typography>
-                    </div>
-                  </div>
-                </div>
-              </BottomNavigation>
-            </Paper>
-          </div>
+            <div style={{ position: 'absolute', right: '0' }}>
+              <FormControlLabel
+                control={
+                  <Switch defaultChecked color="secondary" onClick={() => { getValue() }} />
+                }
+                label={value === true ? "Fahrenheit" : "Celsius"}
+              />
+            </div>
+          </div>}
+        {cityData.main !== undefined ?
+          <Table style={{ marginTop: '3rem', textAlign: 'center' }}>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <Chip label="Temperature" style={{ background: 'black', fontSize: '1rem', color: 'white', width: '9rem' }} />
+                </TableCell>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <Chip label="Minimum" style={{ background: 'black', fontSize: '1rem', color: 'white', width: '9rem' }} />
+                </TableCell>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <Chip label="Maximum" style={{ background: 'black', fontSize: '1rem', color: 'white', width: '9rem' }} />
+                </TableCell>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <PlaceIcon style={{ color: 'red', fontSize: '4rem' }} />
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <Typography style={{ marginLeft: '1rem', fontSize: '3rem' }}>
+                    {temp + degValue}
+                  </Typography>
+                </TableCell>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <Typography style={{ fontSize: '3rem' }}>
+                    {tempMin + degValue}
+                  </Typography>
+                </TableCell>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <Typography style={{ fontSize: '3rem' }}>
+                    {tempMax + degValue}
+                  </Typography>
+                </TableCell>
+                <TableCell style={{ border: 'none', textAlign: 'center' }}>
+                  <h3 style={{ paddingRight: '0.8rem', margin: '0' }}>
+                    {name}
+                  </h3>
+                  <h4 style={{ paddingRight: '0.8rem', margin: '0' }}>
+                    {date.toLocaleString()}
+                  </h4>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
           : <h2 style={{ textAlign: 'center' }}>There is No Data</h2>}
       </div>
     </>
